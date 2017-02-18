df013b26fb0767476d2ced4bd5b35c182103aff4
Eldow <francois.decan@free.fr>
2017-02-17 19:59:38 -0500

    fixed comment

--- scientist/server/routes.js
+++ scientist/server/routes.js
@@ -3,7 +3,7 @@ var Scientist = require('./model');
 
 module.exports = function (app) {
 
-    // GET all scientists - to retrieve the database
+    // GET all scientists
     app.get('/api/scientists', function (req, res) {
       Scientist.find(function (err, scientists) {
           // if there is an error retrieving, send the error. nothing after res.send(err) will execute
@@ -14,7 +14,7 @@ module.exports = function (app) {
       });
     });
 
-    // POST a scientist - to fill the database
+    // GET a scientist by id
     app.get('/api/scientists/:id', function (req, res) {
         Scientist.findById(req.params.id, function (err, scientist) {
             if (err) {
